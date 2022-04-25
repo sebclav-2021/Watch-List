a1b926d6772c681ce7b0251da0258690198b3f72
sebastien clavel <sebclav@hotmail.com>
2022-04-25 10:01:34 +0200

    CRUD lists OK

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,6 +12,36 @@ class ListsController < ApplicationController
   end
 
   def create
-    
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render "edit" # ou :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
